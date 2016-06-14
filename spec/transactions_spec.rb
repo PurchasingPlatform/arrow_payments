f0aa0297375d8c7aa2c148493331a6a4c33d58aa
Gabi Gergely <gabi@purchasingplatform.com>
2016-06-14 16:09:14 -0500

    Fix transaction tests

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -1,5 +1,13 @@
 describe ArrowPayments::Transactions do
   let(:client) { ArrowPayments.client }
+  let(:headers) { {"Accept"=>"application/json", "Content-Type"=>"application/json", "User-Agent"=>"Ruby"} }
+  let(:request) do
+    {
+      body: body,
+      headers: headers
+    }
+  end
+
 
   before :all do
     ArrowPayments::Configuration.api_key = "foobar"
@@ -9,172 +17,166 @@ describe ArrowPayments::Transactions do
 
   describe "#transaction" do
     it "returns a transaction by ID" do
-      stub_api(:get, "/foobar/transaction/40023", :body => fixture("transaction.json"))
+      stub_api(:get, "/foobar/transaction/40023", body: fixture("transaction.json"))
 
-      transaction = client.transaction("40023")
-
-      transaction.should_not be_nil
-      transaction.id.should eq(40023)
+      expect(client.transaction("40023").id).to eq(40023)
     end
 
     it "returns nil if transaction does not exist" do
-      stub_request(:get, "#{api_url}/foobar/transaction/400231").
-        to_return(:status  => 404,
-                  :body    => "",
-                  :headers => {:error => "Transaction Not Found"})
+      stub_request(:get, api_url("/foobar/transaction/400231")).
+        to_return(status: 404, body: "", headers: {error: "Transaction Not Found"})
 
-      client.transaction("400231").should be_nil
+      expect(client.transaction("400231")).to be nil
     end
   end
 
   describe "#transactions" do
     it "raises error on invalid status argument" do
-      expect { client.transactions(10162, "Foo") }.to raise_error "Invalid status: Foo"
+      expect {
+        client.transactions(10162, "Foo")
+      }.to raise_error "Invalid status: Foo"
     end
 
     it "returns unsettled transactions" do
-      stub_request(:get, "#{api_url}/foobar/customer/10162/Transactions/NotSettled").
-        to_return(:status  => 200,
-                  :body    => fixture("transactions.json"),
-                  :headers => {})
+      stub_request(:get, api_url("/foobar/customer/10162/Transactions/NotSettled")).
+        to_return(status: 200, body: fixture("transactions.json"), headers: {})
 
       transactions = client.transactions(10162, "NotSettled")
 
-      transactions.should_not be_empty
-      transactions.size.should eq(2)
-      transactions.map(&:status).uniq.first.should eq("Not Settled")
-    end
-
-    it "returns transactions for status" do
-      pending "API endpoint is not implemented"
+      expect(transactions.size).to eq(2)
+      expect(transactions.map(&:status).uniq.first).to eq("Not Settled")
     end
   end
 
   describe "#create_transaction" do
     let(:payment_information) do
       {
-        :customer_id        => "10162",
-        :payment_method_id  => "0",
-        :transaction_type   => "sale",
-        :total_amount       => 250,
-        :tax_amount         => 0,
-        :shipping_amount    => 0
+        customer_id: "10162",
+        payment_method_id: "0",
+        transaction_type: "sale",
+        total_amount: 250,
+        tax_amount: 0,
+        shipping_amount: 0
       }
     end
 
-    let(:request) do
+    let(:body) do
       {
-        :body => "{\"TransactionType\":\"sale\",\"TotalAmount\":250,\"TransactionSource\":\"API\",\"PaymentMethodID\":\"0\",\"TaxAmount\":0,\"ShippingAmount\":0,\"CustomerID\":\"10162\",\"Amount\":250,\"ApiKey\":\"foobar\",\"MID\":\"foo\"}",
-       :headers => {"Accept"=>"application/json", "Content-Type"=>"application/json", "User-Agent"=>"Ruby"}
+        "TransactionType" => "sale",
+        "TotalAmount" => 250,
+        "TransactionSource" => "API",
+        "PaymentMethodID" => "0",
+        "TaxAmount" => 0,
+        "ShippingAmount" => 0,
+        "CustomerID" => "10162",
+        "Amount" => 250,
+        "ApiKey" => "foobar",
+        "MID" => "foo"
       }
     end
 
     it "creates a new transaction record" do
-      stub_request(:post, "#{api_url}/transaction/add").with(request).
-        to_return(:status  => 200,
-                  :body    => fixture("transaction.json"),
-                  :headers => {})
+      stub_request(:post, api_url("/transaction/add")).with(request).
+        to_return(status: 200, body: fixture("transaction.json"), headers: {})
 
       transaction = client.create_transaction(payment_information)
-      transaction.should_not be_nil
-      transaction.customer_name.should eq("First Supplies")
-      transaction.authorization_code.should eq("123456")
-      transaction.status.should eq("Not Settled")
+      expect(transaction).to_not be nil
+      expect(transaction.customer_name).to eq("First Supplies")
+      expect(transaction.authorization_code).to eq("123456")
+      expect(transaction.status).to eq("Not Settled")
     end
 
     it "raises an error if the payment method is not found" do
-      stub_request(:post, "#{api_url}/transaction/add").with(request).
-        to_return(:status => 404,
-                  :body => "",
-                  :headers => {:error => "Payment Method Not Found"})
+      stub_request(:post, api_url("/transaction/add")).with(request).
+        to_return(status: 404, body: "", headers: {error: "Payment Method Not Found"})
 
-      expect {client.create_transaction(payment_information) }.to raise_error "Payment Method Not Found"
+      expect {
+        client.create_transaction(payment_information)
+      }.to raise_error "Payment Method Not Found"
     end
   end
 
   describe "#capture_transaction" do
-    let(:request) do
+    let(:body) do
       {
-        :body    => "{\"TransactionId\":10162,\"Amount\":100,\"ApiKey\":\"foobar\",\"MID\":\"foo\"}",
-        :headers => {"Accept"=>"application/json","Content-Type"=>"application/json","User-Agent"=>"Ruby"}
+        "TransactionId" => 10162,
+        "Amount" => 100,
+        "ApiKey" => "foobar",
+        "MID" => "foo"
       }
     end
 
     it "raises error if transaction does not exist" do
-      stub_request(:post, "#{api_url}/transaction/capture").with(request).
-        to_return(:status  => 404,
-                  :body    => "",
-                  :headers => {:error => "Transaction Not Found"})
+      stub_request(:post, api_url("/transaction/capture")).with(request).
+        to_return(status: 404, body: "", headers: {error: "Transaction Not Found"})
 
-      expect { client.capture_transaction(10162, 100) }.to raise_error "Transaction Not Found"
+      expect {
+        client.capture_transaction(10162, 100)
+      }.to raise_error "Transaction Not Found"
     end
 
     it "captures transaction for amount" do
-      stub_request(:post, "#{api_url}/transaction/capture").with(request).
-        to_return(:status  => 200,
-                  :body    => fixture("transaction_capture.json"),
-                  :headers => {})
+      stub_request(:post, api_url("/transaction/capture")).with(request).
+        to_return(status: 200, body: fixture("transaction_capture.json"), headers: {})
 
-      client.capture_transaction(10162, 100).should be_true
+      expect(client.capture_transaction(10162, 100)).to be true
     end
 
     it "raises a error if amount is greater than original" do
+      msg = "Unable to Capture for more than the original amount of $10.00"
+      stub_request(:post, api_url("/transaction/capture")).with(request).
+        to_return(status: 400, body: "", headers: {error: msg})
 
-      stub_request(:post, "#{api_url}/transaction/capture").with(request).
-        to_return(:status  => 400,
-                  :body    => "",
-                  :headers => {:error => "Unable to Capture for more than the original amount of $10.00"})
-
-      expect { client.capture_transaction(10162, 100) }.to raise_error "Unable to Capture for more than the original amount of $10.00"
+      expect {
+        client.capture_transaction(10162, 100)
+      }.to raise_error msg
     end
 
     it "raises an error if transaction is already captured" do
-      stub_request(:post, "#{api_url}/transaction/capture").with(request).
-        to_return(:status  => 400,
-                  :body    => "",
-                  :headers => {:error => "Transaction is Captured Already"})
+      stub_request(:post, api_url("/transaction/capture")).with(request).
+        to_return(status: 400, body: "", headers: {error: "Transaction is Captured Already"})
 
-      expect { client.capture_transaction(10162, 100) }.to raise_error "Transaction is Captured Already"
+      expect {
+        client.capture_transaction(10162, 100)
+      }.to raise_error "Transaction is Captured Already"
     end
   end
 
   describe "#void_transaction" do
-
-    let(:request) do
+    let(:body) do
       {
-        :body    => "{\"TransactionId\":10162,\"ApiKey\":\"foobar\",\"MID\":\"foo\"}",
-        :headers => {"Accept"=>"application/json", "Content-Type"=>"application/json", "User-Agent"=>"Ruby"}
+        "TransactionId" => 10162,
+        "ApiKey" => "foobar",
+        "MID" => "foo"
       }
     end
 
     context "successful request" do
       it "voids transaction" do
-        stub_request(:post, "#{api_url}/transaction/void").with(request).
-          to_return(:status  => 200,
-                    :body    => fixture("void_transaction.json"),
-                    :headers => {})
+        stub_request(:post, api_url("/transaction/void")).with(request).
+          to_return(status: 200, body: fixture("void_transaction.json"), headers: {})
 
-        client.void_transaction(10162).should be_true
+        expect(client.void_transaction(10162)).to be true
       end
     end
 
     context "unsuccessful requests" do
       it "raises an error if transaction does not exist" do
-        stub_request(:post, "#{api_url}/transaction/void").with(request).
-          to_return(:status  => 404,
-                    :body    => "",
-                    :headers => {:error => "Transaction Not Found"})
+        stub_request(:post, api_url("/transaction/void")).with(request).
+          to_return(status: 404, body: "", headers: {error: "Transaction Not Found"})
 
-        expect { client.void_transaction(10162) }.to raise_error "Transaction Not Found"
+        expect {
+          client.void_transaction(10162)
+        }.to raise_error "Transaction Not Found"
       end
 
       it "raises an error if transaction is already voided" do
-        stub_request(:post, "#{api_url}/transaction/void").with(request)
-          .to_return(:status  => 400,
-                     :body    => "",
-                     :headers => {:error => "Transaction Already Void"})
+        stub_request(:post, api_url("/transaction/void")).with(request)
+          .to_return(status: 400, body: "", headers: {error: "Transaction Already Void"})
 
-        expect { client.void_transaction(10162) }.to raise_error "Transaction Already Void"
+        expect {
+          client.void_transaction(10162)
+        }.to raise_error "Transaction Already Void"
       end
     end
   end
