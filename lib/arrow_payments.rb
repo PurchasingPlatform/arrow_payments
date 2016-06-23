a0d3a9beb75adc4daab4728fb8906b1d2bd99bfd
Gabi Gergely <gabi@purchasingplatform.com>
2016-06-23 14:40:55 -0500

    Revert removal of second arg in autoload

--- lib/arrow_payments.rb
+++ lib/arrow_payments.rb
@@ -2,19 +2,19 @@ require "arrow_payments/version"
 require "arrow_payments/errors"
 
 module ArrowPayments
-  autoload :Address
-  autoload :Client
-  autoload :Configuration
-  autoload :Connection
-  autoload :Customer
-  autoload :Customers
-  autoload :Entity
-  autoload :LineItem
-  autoload :PaymentMethod
-  autoload :PaymentMethods
-  autoload :RecurringBilling
-  autoload :Transaction
-  autoload :Transactions
+  autoload :Address, "arrow_payments/address"
+  autoload :Client, "arrow_payments/client"
+  autoload :Configuration, "arrow_payments/configuration"
+  autoload :Connection, "arrow_payments/connection"
+  autoload :Customer, "arrow_payments/customer"
+  autoload :Customers, "arrow_payments/client/customers"
+  autoload :Entity, "arrow_payments/entity"
+  autoload :LineItem, "arrow_payments/line_item"
+  autoload :PaymentMethod, "arrow_payments/payment_method"
+  autoload :PaymentMethods, "arrow_payments/client/payment_methods"
+  autoload :RecurringBilling, "arrow_payments/recurring_billing"
+  autoload :Transaction, "arrow_payments/transaction"
+  autoload :Transactions, "arrow_payments/client/transactions"
 
   class << self
     def client(options=nil)
